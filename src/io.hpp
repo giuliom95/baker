70038eacdf02ddcf348d374b02a1aa1483045283
giuliom95 <giuliomartella1995@gmail.com>
2018-09-22 19:29:52 +0200

    EXR save function

---
+++ src/io.hpp
@@ -0,0 +1,19 @@
+#ifndef IO_HPP
+#define IO_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <OpenEXR/ImfRgbaFile.h>
+#include <OpenEXR/ImfRgba.h>
+
+#include "math.hpp"
+
+namespace io {
+    void saveEXR(   const std::string& path,
+                    const int w, const int h, 
+                    const std::vector<Vec4h>& image);
+}
+
+#endif
