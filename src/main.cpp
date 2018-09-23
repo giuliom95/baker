0a3011934def053d6580171793e5214c1c14854c
giuliom95 <giuliomartella1995@gmail.com>
2018-09-23 20:12:04 +0200

    UV rendering

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char* argv[]) {
 	const auto embree_scene		= rtcNewScene(embree_device);
 
 	/** Start building scene **/
-	const auto model = io::load_obj("in/plane.obj");
+	const auto model = io::load_obj("in/cube.obj");
 
 	const auto embree_geom = rtcNewGeometry(embree_device, RTC_GEOMETRY_TYPE_TRIANGLE);
 	rtcSetSharedGeometryBuffer(embree_geom, RTC_BUFFER_TYPE_VERTEX, 0, RTC_FORMAT_FLOAT3, model.vtxs.data(), 0, sizeof(Vec3f), model.vtxs.size());
@@ -36,30 +36,44 @@ int main(int argc, char* argv[]) {
 
 	/** Start scene intersection **/
 
-	for (auto j = -1; j <= 5; j += 2) {
-		for (auto i = -1; i <= 5; i += 2) {
+	const auto img_w = 300;
+	const auto img_h = 300;
+	std::vector<Vec4h> img(img_w*img_h, Vec4h());
+
+	for (auto j = 0; j < img_h; ++j) {
+		for (auto i = 0; i < img_w; ++i) {
 			RTCIntersectContext ray_context;
 			rtcInitIntersectContext(&ray_context);
 
 			RTCRayHit ray_hit;
-			ray_hit.ray.org_x = i * 0.25f + .1f; ray_hit.ray.org_y = j * 0.25f + .1f; ray_hit.ray.org_z = 3.0f;
+			ray_hit.ray.org_x = 2.f*i/img_w - .5f; ray_hit.ray.org_y = 2.f*j/img_h - .5f; ray_hit.ray.org_z = 3.0f;
 			ray_hit.ray.dir_x = 0.0f; ray_hit.ray.dir_y = 0.0f; ray_hit.ray.dir_z = -1.0f;
 			ray_hit.ray.tnear = 0.0f; ray_hit.ray.tfar = 100.0f; 
 			ray_hit.ray.flags = 0;
 			ray_hit.hit.instID[0] = RTC_INVALID_GEOMETRY_ID;
 			ray_hit.hit.geomID = RTC_INVALID_GEOMETRY_ID;
-			std::cout << ray_hit.ray.org_x << ", " << ray_hit.ray.org_y << ": ";
+			// std::cout << ray_hit.ray.org_x << ", " << ray_hit.ray.org_y << ": ";
 			rtcIntersect1(embree_scene, &ray_context, &ray_hit);
-			std::cout << (signed) ray_hit.hit.geomID << " -- ";
-			std::cout << (signed) ray_hit.ray.tfar << std::endl;
+			// std::cout << (signed) ray_hit.hit.geomID << " -- ";
+			// std::cout << (signed) ray_hit.ray.tfar << std::endl;
 			if(ray_hit.hit.geomID == geom_id) {
 				const auto tri_id = ray_hit.hit.primID;
-				std::cout << "## " << tri_id << ": ";
-				std::cout << ray_hit.hit.u << " ";
-				std::cout << ray_hit.hit.v << std::endl;
+				// std::cout << "## " << tri_id << ": ";
+				// std::cout << ray_hit.hit.u << " ";
+				// std::cout << ray_hit.hit.v << std::endl;
+
+				const auto uv_tri = model.uvtris[tri_id];
+				const auto gamma = 1 - ray_hit.hit.u - ray_hit.hit.v;
+				const auto texuv = gamma * model.uvs[uv_tri[0]] + ray_hit.hit.u * model.uvs[uv_tri[1]] + ray_hit.hit.v * model.uvs[uv_tri[2]];
+				// std::cout << texuv << std::endl;
+				img[i + img_w*j][0] = texuv[0];
+				img[i + img_w*j][1] = texuv[1];
+				img[i + img_w*j][3] = 1.0;
 			}
 		}
 	}
+
+	io::save_exr("./test.exr", img_w, img_h, img);
 	/** End scene intersection **/
 
 	
